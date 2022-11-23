65d8a27b6a106da91a3108d7ed6a0f68e80e8085
thanmaisai <91474486+thanmaisai@users.noreply.github.com>
2022-11-23 20:20:55 +0530

    Created Frontend

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# Turoria
+Save Planet Earth<br>
+All of it starts from you!
+# [Greensavers](https://black-hill-6592.on.fleek.co/)
+A website and a cross-flatform App made for the <strong>Hackoheist</strong> 2022 Hackathon
+
+
+The problem Greensavers solves
+Even with strict regulations against industries and institutions to protect the environment, the government can not track everybody. Environmental NGOs fill this gap by monitoring the activites and reporting the concerned authotithies.
+But even they are limited to how much environmentally damaging activities they can investigate.
+
+This is where the role of a comman being or a resident comes in. A resident can spot such activity, but often cant take any action due to many reasons.
+
+To Fix this issue, we have made a decentralised website, which is hosted on IPFS, so anybody, including big industries will have a hard time taking it down. The User can raise an issue in the website by providing all neccessary details. Once the issue is logged, it is visible to the NGO, which can furhter get invovled in the case.
+
+All files related to the Issue is stored on IPFS, and the frontend for the website itself is hosted on IPFS
+We have also made a app wrapper in flutter, so it can be used on ios, android, windows, linux and macos too.
+
+You can Find the PPT [here](https://bit.ly/3A4Asdo)
+you can find the demo [here](https://youtu.be/agXyUSfSdAk)
+
+
+## Development
+Endpoint for the [front-end](https://black-hill-6592.on.fleek.co/) 
+<br>
+Endpoint for the [back-end](https://hackoheist.el.r.appspot.com/)
+
+# Tech Stack Used
+- [IPFS](https://docs.ipfs.tech/)
+- [Google Cloud](https://cloud.google.com/)
+- [Flutter](https://flutter.dev/)
+- [Node JS](https://nodejs.org/en/)
+- [Bulma](https://bulma.io/)
+- [File coin](https://filecoin.io/)
+- [Fleek](https://fleek.co/)
+
+## Installation
+No worries, we got it covered <br> just visit our site 
+## Screenshots
+<img width="1429" alt="Screenshot 2022-08-09 at 11 42 26 AM" src="https://user-images.githubusercontent.com/78999739/183580016-86d27243-f3fc-472f-bd6e-daf56c0647ad.png">
+
+<img width="1429" alt="Screenshot 2022-08-09 at 11 42 38 AM" src="https://user-images.githubusercontent.com/78999739/183579998-20484c82-11ef-42af-9ec6-2b4c97590d3a.png">
+
+![WhatsApp Image 2022-08-09 at 11 53 55 AM](https://user-images.githubusercontent.com/78999739/183579965-e0e4446b-1ac1-49d4-ac2d-135ede5c1cc7.jpeg)
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,234 @@
+var express = require('express');
+var cors = require('cors')
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const multer = require('multer')
+var fs = require('fs');
+// const upload = multer({ dest: './public/temp/uploads/' })
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
+const fleek = require('@fleekhq/fleek-storage-js');
+require('dotenv').config()
+const apiKey = process.env.FLEEK_API_KEY;
+const apiSecret = process.env.FLEEK_API_SECRET;
+
+
+var app = express();
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+app.use(cors())
+
+
+app.get('/', function (req, res) {
+    res.send('hello, this is just a backend. The link for the frontend will be added here soon')
+})
+
+
+
+
+
+const uploadFunction = async (data) => {
+    const date = new Date();
+    const timestamp = date.getTime();
+
+    const input = {
+        apiKey,
+        apiSecret,
+        key: `${timestamp}.txt`,
+        data,
+    };
+
+    try {
+        const result = await fleek.upload(input);
+        console.log(result);
+    } catch (e) {
+        console.log('error', e);
+    }
+}
+
+const uploadPicFunction = async (data, ContentType, key) => {
+
+    const input = {
+        apiKey,
+        apiSecret,
+        key,
+        data,
+        ContentType
+    };
+
+    try {
+        const result = await fleek.upload(input);
+        console.log(result);
+    } catch (e) {
+        console.log('error', e);
+    }
+}
+
+async function deleteAllData() {
+    try {
+        const result = await fleek.deleteFile({
+            apiKey,
+            apiSecret,
+            key: 'alldata.json',
+            bucket: '1e4f9433-e9a2-4412-a561-9a1ddf54e93c-bucket',
+        });
+    } catch (e) {
+        console.log('error', e);
+    }
+
+}
+async function alldataUpload(data) {
+    const input = {
+        apiKey,
+        apiSecret,
+        key: 'alldata.json',
+        data,
+    };
+
+    try {
+        const result = await fleek.upload(input);
+    } catch (e) {
+        console.log('error', e);
+    }
+}
+
+app.post('/createComplaint', upload.single('upfile'), (req, res) => {
+    const fileName = req.file.originalname;
+    const fileType = req.file.mimetype;
+    const fileSize = req.file.size;
+    const theFile = req.file.buffer;
+    var userName = req.body.name;
+    var userEmail = req.body.email;
+    var location = req.body.location;
+    var description = req.body.description;
+    const date = new Date();
+    const PicKey = date.getTime() + '.png';
+    var theElement = {
+        userName,
+        userEmail,
+        location,
+        description,
+        PicKey,
+        status: 0
+    }
+    async function getDB() {
+
+
+        const res = await fetch('https://storageapi.fleek.co/1e4f9433-e9a2-4412-a561-9a1ddf54e93c-bucket/alldata.json')
+
+        alldata = await res.json();
+        // alldata.push(theElement);
+        alldata['issues'].push(theElement)
+
+
+        // delte old file and create new
+        await deleteAllData()
+        // upload alldata
+        await alldataUpload(JSON.stringify(alldata));
+
+        await uploadPicFunction(theFile, fileType, PicKey);
+
+    }
+    getDB();
+
+    res.redirect('https://black-hill-6592.on.fleek.co/success.html');
+
+
+    // uploadFunction(userName);
+    // uploadFunction(userEmail);
+    // uploadFunction(location);
+    // uploadFunction(description);
+
+
+
+
+
+})
+
+
+app.post('/updateStatus', function (req, res) {
+    const id = req.body.idbtn;
+    const status = req.body.submitButton;
+
+
+    // alldata = JSON.parse(alldata)
+    async function getDB() {
+        const res = await fetch('https://storageapi.fleek.co/1e4f9433-e9a2-4412-a561-9a1ddf54e93c-bucket/alldata.json')
+
+        alldata = await res.json();
+        
+        if (status == 'Close') {
+            alldata['issues'][id].status = 2;
+        } else if (status == 'Investigating') {
+            alldata['issues'][id].status = 1;
+        }
+
+        await deleteAllData()
+        await alldataUpload(JSON.stringify(alldata));
+
+    }
+
+    getDB()
+    res.redirect('https://black-hill-6592.on.fleek.co/success.html');
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+
+    console.log('Your app is listening on port \n' + 'http://localhost:' + port)
+});
